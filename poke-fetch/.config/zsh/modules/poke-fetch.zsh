f2dd35abfafa51e28ad3a6ffc131949b3b4438c6
Valentin <valentinchantelot@protonmail.com>
2025-10-01 13:56:57 +0200

    Fix typo

--- poke-fetch/.config/zsh/modules/poke-fetch.zsh
+++ poke-fetch/.config/zsh/modules/poke-fetch.zsh
@@ -1,5 +1,5 @@
 # Display a random pokemon when openning a new terminal
-# Homemade script, visible in /dotfiles or on Github here https://github.com/valentinChantelot/poke-fetch
+# Homemade script. See it in /dotfiles or on Github here : https://github.com/valentinChantelot/poke-fetch
 
 # Do not execute in non interactive shell
 [[ $- != *i* ]] && exit 0
