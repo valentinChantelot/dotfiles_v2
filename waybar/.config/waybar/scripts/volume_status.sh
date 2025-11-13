17a01ff8d602941c4a1ed1ed7ef69b1f383552bc
Valentin <valentinchantelot@protonmail.com>
2025-11-13 15:24:54 +0100

    Add clock and date config

--- waybar/.config/waybar/scripts/volume_status.sh
+++ waybar/.config/waybar/scripts/volume_status.sh
@@ -1,26 +1,27 @@
 #!/usr/bin/env bash
 
-VOLUME=$(pactl get-sink-volume @DEFAULT_SINK@ | awk '{print $5}' | tr -d '%')
-MUTED=$(pactl get-sink-mute @DEFAULT_SINK@ | awk '{print $2}')
+get_status() {
+  VOLUME=$(pactl get-sink-volume @DEFAULT_SINK@ | awk '{print $5}' | tr -d '%')
+  MUTED=$(pactl get-sink-mute @DEFAULT_SINK@ | awk '{print $2}')
 
-if [ -z "$VOLUME" ]; then
-  echo '{"class": "muted"}'
-  exit 0
-fi
+  if [ "$MUTED" = "yes" ] || [ "$VOLUME" -eq 0 ]; then
+    echo '{"class": "muted"}'
+  elif [ "$VOLUME" -lt 10 ]; then
+    echo '{"class": "volume-0"}'
+  elif [ "$VOLUME" -lt 25 ]; then
+    echo '{"class": "volume-1"}'
+  elif [ "$VOLUME" -lt 40 ]; then
+    echo '{"class": "volume-2"}'
+  else
+    echo '{"class": "volume-3"}'
+  fi
+}
 
-if [ "$MUTED" = "yes" ]; then
-  echo '{"class": "muted"}'
-  exit 0
-fi
+get_status
 
-if [ "$VOLUME" -eq 0 ]; then
-  echo '{"class": "muted"}'
-elif [ "$VOLUME" -lt 10 ]; then
-  echo '{"class": "volume-0"}'
-elif [ "$VOLUME" -lt 25 ]; then
-  echo '{"class": "volume-1"}'
-elif [ "$VOLUME" -lt 40 ]; then
-  echo '{"class": "volume-2"}'
-else
-  echo '{"class": "volume-3"}'
-fi
+# listen pipewire and pulseaudio events
+pactl subscribe | while read -r event; do
+  if   echo "$event" | grep -q "sink"; then
+    get_status
+  fi
+done
